1a6ef16eecf2d778d7617c4591e07e82265df4c4
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2022-12-22 23:45:30 +0100

    correct linter errors

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
-const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2');
-const scale = document.querySelector('.scale');
-const btn2 = document.querySelector('.btn2');
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const scale = document.querySelector('.scale')
+const btn2 = document.querySelector('.btn2')
 
 function myFunction () {
   const unable = document.getElementById('input1').disabled;
@@ -10,21 +10,21 @@ function myFunction () {
 }
 
 btn2.onclick = function getOption () {
-  const x = (input1.value).split(',');
+  const x = (input1.value).split(',')
   const weight1 = parseInt(x[0])
   const weight2 = parseInt(x[1])
 
-  const n = (input2.value).split(',');
-  console.log(n);
+  const n = (input2.value).split(',')
+  console.log(n)
 
   for (let i = 0; i < n.length; i++) {
     for (let j = 0; j < n.length; j++) {
       if (weight1 === weight2) {
-        scale.innerHTML = 'Balance';
+        scale.innerHTML = 'Balance'
       } else if (weight1 + parseInt(n[2]) === weight2 + parseInt(n[1])) {
-        scale.innerHTML = 'Balance';
+        scale.innerHTML = 'Balance'
       } else {
-        scale.innerHTML = 'Scale Imbalanced';
+        scale.innerHTML = 'Scale Imbalanced'
       }
     }
   }
