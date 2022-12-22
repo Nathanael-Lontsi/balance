8c4f7b32b0af2ce3d882a135e25238567eb1bec7
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2022-12-22 12:46:14 +0100

    new data input

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+let input1 = document.getElementById('#input1');
+let input2 = document.getElementById('#input2');
+let scale = document.querySelector('.scale');
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+
+btn1.onclick = function getOption () {
+    
+}
+
+btn2.onclick = function getOption () {
+    
+}
