f9268e723240171f60717092c3c929e931f49001
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2022-12-22 20:08:10 +0100

    add several tags

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,30 @@
-let input1 = document.getElementById('#input1');
-let input2 = document.getElementById('#input2');
-let scale = document.querySelector('.scale');
-let btn1 = document.querySelector('.btn1');
-let btn2 = document.querySelector('.btn2');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const scale = document.querySelector('.scale');
+const btn2 = document.querySelector('.btn2');
 
-btn1.onclick = function getOption () {
-    
+function myFunction() {
+  const unable = document.getElementById('input1').disabled = true;
+  document.getElementById('input2').disabled = true;
 }
 
-btn2.onclick = function getOption () {
-    
+btn2.onclick = function getOption() {
+  const x = (input1.value).split(',');
+  const weight1 = parseInt(x[0])
+  const weight2 = parseInt(x[1])
+
+  const n = (input2.value).split(',');
+  console.log(n);
+
+  for (let i = 0; i < n.length; i++) {
+    for (let j = 0; j < n.length; j++) {
+        if (weight1 === weight2) {
+                scale.innerHTML = 'Balance';
+            } else if (weight1 + parseInt(n[2]) === weight2 + parseInt(n[1])) {
+                scale.innerHTML = 'Balance';
+            } else {
+                scale.innerHTML = 'Scale Imbalanced';
+            }
+        }
+    }
 }
